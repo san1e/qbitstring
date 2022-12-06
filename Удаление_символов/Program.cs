e11496b4a8698085fc46d8175ac9ba9fde957a50
sashaklymenko <alklimenko227@outlook.com>
2022-12-06 22:43:13 +0200

    удаление символов

---
+++ Удаление_символов/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Удаление_символов
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] array = Console.ReadLine().Trim().Split();
+            int n = (int.Parse(array[0])) - 1;
+            int m = int.Parse(array[1]);
+            Console.WriteLine(text.Remove(n, m - n));
+
+        }
+    }
+}
