865305aeecffaa619f16d7c8af980e3334af96dd
sashaklymenko <alklimenko227@outlook.com>
2022-12-14 22:55:06 +0200

    3

---
+++ К-й_пробел/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Кй_пробел
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            int counter = 0;
+            for (int i = 0; i < str.Length && counter != number; i++)
+            {
+                if (Char.IsWhiteSpace(str, i))
+                {
+                    counter++;
+                    result = i;
+                }
+            }
+            if (counter < number)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(result + 1);
+            }
+        }
+    }
+}
---
+++ Образец_текст/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+namespace Образец_текст
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           string P =Console.ReadLine();
+            string T = Console.ReadLine();
+            int k = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            int count = 0;
+           while (i < P.Length)
+            {
+                count++;
+                if (P[i] == T[k-1])
+                {
+                    i++;
+                    k++;
+                   
+                }
+                else
+                {
+                    
+                    k++;
+                    break;
+                }
+            }
+            if (T.Contains(P))
+            {
+                global::System.Console.WriteLine("YES");
+                global::System.Console.WriteLine(count);
+            }
+            else
+            {
+                global::System.Console.WriteLine("NO");
+                global::System.Console.WriteLine(count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
